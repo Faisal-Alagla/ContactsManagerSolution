3f826f3781c3463dda96266952cb02c209c21a9b
Faisal <Faisal@DESKTOP-70EEDMP>
2024-02-16 18:34:28 +0300

    Migrating the tests

---
+++ ContactsManager.ControllerTests/PersonsControllerTest.cs
@@ -0,0 +1,137 @@
+﻿using AutoFixture;
+using Moq;
+using ServiceContracts;
+//using Xunit;
+using FluentAssertions;
+using CRUD_Example.Controllers;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ContactsManager.Core.ServiceContracts.PersonsServiceContracts;
+
+namespace Tests_Example
+{
+    public class PersonsControllerTest
+    {
+        private readonly IPersonsGetterService _personsGetterService;
+        private readonly IPersonsAdderService _personsAdderService;
+        private readonly IPersonsUpdaterService _personsUpdaterService;
+        private readonly IPersonsSorterService _personsSorterService;
+        private readonly IPersonsDeleterService _personsDeleterService;
+        private readonly ICountriesService _countriesService;
+        private readonly ILogger<PersonsController> _logger;
+
+        private readonly Mock<IPersonsGetterService> _personsGetterServiceMock;
+        private readonly Mock<IPersonsAdderService> _personsAdderServiceMock;
+        private readonly Mock<IPersonsUpdaterService> _personsUpdaterServiceMock;
+        private readonly Mock<IPersonsSorterService> _personsSorterServiceMock;
+        private readonly Mock<IPersonsDeleterService> _personsDeleterServiceMock;
+        private readonly Mock<ICountriesService> _countriesServiceMock;
+        private readonly Mock<ILogger<PersonsController>> _loggerMock;
+
+        private readonly Fixture _fixture;
+
+        public PersonsControllerTest()
+        {
+            _fixture = new Fixture();
+
+            _personsGetterServiceMock = new Mock<IPersonsGetterService>();
+            _personsAdderServiceMock = new Mock<IPersonsAdderService>();
+            _personsUpdaterServiceMock = new Mock<IPersonsUpdaterService>();
+            _personsSorterServiceMock = new Mock<IPersonsSorterService>();
+            _personsDeleterServiceMock = new Mock<IPersonsDeleterService>();
+            _countriesServiceMock = new Mock<ICountriesService>();
+            _loggerMock = new Mock<ILogger<PersonsController>>();
+
+            _personsGetterService = _personsGetterServiceMock.Object;
+            _personsAdderService = _personsAdderServiceMock.Object;
+            _personsUpdaterService = _personsUpdaterServiceMock.Object;
+            _personsSorterService = _personsSorterServiceMock.Object;
+            _personsDeleterService = _personsDeleterServiceMock.Object;
+            _countriesService = _countriesServiceMock.Object;
+            _logger = _loggerMock.Object;
+        }
+
+        #region Index
+
+        [Fact]
+        public async Task Index_ShouldReturnIndexViewWithPersonsList()
+        {
+            //Arrange
+            List<PersonResponse> persons_response_list = _fixture.Create<List<PersonResponse>>();
+
+            PersonsController personsController = new PersonsController(_personsGetterService, _personsAdderService, _personsUpdaterService, _personsSorterService, _personsDeleterService, _countriesService, _logger);
+
+            _personsGetterServiceMock.Setup(temp => temp.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(persons_response_list);
+
+            _personsSorterServiceMock.Setup(temp => temp.GetSortedPersons(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>()))
+                .ReturnsAsync(persons_response_list);
+
+            //Act
+            IActionResult result = await personsController.Index(_fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<string>(), _fixture.Create<SortOrderOptions>());
+
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            viewResult.ViewData.Model.Should().BeAssignableTo<IEnumerable<PersonResponse>>();
+            viewResult.ViewData.Model.Should().Be(persons_response_list);
+        }
+
+        #endregion
+
+        #region Create
+
+        //The following test case is invalid due to moving the model state check into a filter (can test the filter instead)
+        //[Fact]
+        //public async Task Create_IfModelErrors_ToReturnCreateView()
+        //{
+        //    //Arrange
+        //    PersonAddRequest person_add_request = _fixture.Create<PersonAddRequest>();
+        //    PersonResponse person_response = _fixture.Create<PersonResponse>();
+        //    List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+        //    _countriesServiceMock.Setup(temp => temp.GetAllCountries())
+        //        .ReturnsAsync(countries);
+
+        //    _personsServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>()))
+        //        .ReturnsAsync(person_response);
+
+        //    PersonsController personsController = new PersonsController(_personsService, _countriesService, _logger);
+
+        //    //Act
+        //    personsController.ModelState.AddModelError("PersonName", "Cannot be blank");
+        //    IActionResult result = await personsController.Create(person_add_request);
+
+        //    //Assert
+        //    ViewResult viewResult = Assert.IsType<ViewResult>(result);
+        //    //viewResult.ViewData.Model.Should().BeAssignableTo<PersonAddRequest>();
+        //    //viewResult.ViewData.Model.Should().Be(person_add_request);
+        //}
+        [Fact]
+        public async Task Create_IfNoModelErrors_ToReturnIndexView()
+        {
+            //Arrange
+            PersonAddRequest person_add_request = _fixture.Create<PersonAddRequest>();
+            PersonResponse person_response = _fixture.Create<PersonResponse>();
+            List<CountryResponse> countries = _fixture.Create<List<CountryResponse>>();
+
+            _countriesServiceMock.Setup(temp => temp.GetAllCountries())
+                .ReturnsAsync(countries);
+
+            _personsAdderServiceMock.Setup(temp => temp.AddPerson(It.IsAny<PersonAddRequest>()))
+                .ReturnsAsync(person_response);
+
+            PersonsController personsController = new PersonsController(_personsGetterService, _personsAdderService, _personsUpdaterService, _personsSorterService, _personsDeleterService, _countriesService, _logger);
+
+            //Act
+            IActionResult result = await personsController.Create(person_add_request);
+
+            //Assert
+            RedirectToActionResult redirectResult = Assert.IsType<RedirectToActionResult>(result);
+            redirectResult.ActionName.Should().Be("Index");
+        }
+
+        #endregion
+    }
+}
---
+++ ContactsManager.ServiceTests/CountriesServiceTest.cs
@@ -0,0 +1,172 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using Moq;
+
+namespace CRUD_Tests
+{
+    /*
+	 These tests are commented due to them being old (before migrating to the clean architecture)
+	Need to change them the same way as in PersonsServiceTest.cs in order for them to work
+	 */
+
+    /*
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService _countriesService;
+
+        public CountriesServiceTest()
+        {
+            var countriesItitalData = new List<Country>();
+            DbContextMock<ApplicationDbContext> dbContextMock = new DbContextMock<ApplicationDbContext>(
+                new DbContextOptionsBuilder<ApplicationDbContext>().Options
+            );
+
+            ApplicationDbContext dbContext = dbContextMock.Object;
+            dbContextMock.CreateDbSetMock(temp => temp.Countries, countriesItitalData);
+
+            _countriesService = new CountriesService(null);
+        }
+
+        #region AddCountry
+        //When CountryAddRequest is null, it sould throw ArgumentNullException
+        [Fact]
+        public async Task AddCountry_NullCountry()
+        {
+            //Arrange
+            CountryAddRequest? request = null;
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                //Act
+                await _countriesService.AddCountry(request);
+            });
+        }
+
+        //When CountryName is null, it should throw ArgumentException
+        [Fact]
+        public async Task AddCountry_CountryNameNull()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest() { CountryName = null };
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                //Act
+                await _countriesService.AddCountry(request);
+            });
+        }
+
+        //When CountryName is a duplicate, it should throw ArgumentException[Fact]
+        [Fact]
+        public async Task AddCountry_CountryNameDuplicate()
+        {
+            //Arrange
+            CountryAddRequest? request1 = new CountryAddRequest() { CountryName = "KSA" };
+            CountryAddRequest? request2 = new CountryAddRequest() { CountryName = "KSA" };
+
+            //Assert
+            await Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                //Act
+                await _countriesService.AddCountry(request1);
+                await _countriesService.AddCountry(request2);
+            });
+        }
+
+        //When proper CountryName is supplied, it should be added to the list of countries
+        [Fact]
+        public async Task AddCountry_ProperCountryDetails()
+        {
+            //Arrange
+            CountryAddRequest? request = new CountryAddRequest() { CountryName = "KSA" };
+
+            //Act
+            CountryResponse response = await _countriesService.AddCountry(request);
+            List<CountryResponse> countries_from_GetAllCountries = await _countriesService.GetAllCountries();
+
+            //Assert
+            Assert.True(response.CountryID != Guid.Empty);
+            Assert.Contains(response, countries_from_GetAllCountries);
+        }
+        #endregion
+
+        #region GetAllCountries
+        //List of countries should be empty by default
+        [Fact]
+        public async Task GetAllCountries_EmptyList()
+        {
+            //Acts
+            List<CountryResponse> actual_country_response_list = await _countriesService.GetAllCountries();
+
+            //Assert
+            Assert.Empty(actual_country_response_list);
+        }
+
+        //
+        [Fact]
+        public async Task GetAllCountries_AddFewCountries()
+        {
+            //Arrange
+            List<CountryAddRequest> country_request_list = new List<CountryAddRequest>()
+            {
+                new CountryAddRequest() { CountryName = "KSA"},
+                new CountryAddRequest() { CountryName = "DE"},
+            };
+
+            //Act
+            List<CountryResponse> countries_list_from_add_country = new List<CountryResponse>();
+            foreach (CountryAddRequest country_request in country_request_list)
+            {
+                countries_list_from_add_country.Add(await _countriesService.AddCountry(country_request));
+            }
+
+            List<CountryResponse> actualCountryResponseList = await _countriesService.GetAllCountries();
+
+            //read each element from countries_list_from_add_country
+            foreach (CountryResponse expected_country in countries_list_from_add_country)
+            {
+                Assert.Contains(expected_country, actualCountryResponseList);
+            }
+        }
+        #endregion
+
+        #region GetCountryByCountryID
+        //If we supply null as CountryID, it should return null as CountryResponse
+        [Fact]
+        public async Task GetCountryByCountryID_NullCountryID()
+        {
+            //Arrange
+            Guid? countryID = null;
+
+            //Act
+            CountryResponse? country_response_from_get_method = await _countriesService.GetCountryByCountryID(countryID);
+
+            //Assert
+            Assert.Null(country_response_from_get_method);
+        }
+
+        //If we supply a valid CountryID, it should return the matching country details as CountryResponse object
+        [Fact]
+        public async Task GetCountryByCountryID_ValidCountryID()
+        {
+            //Arrange
+            CountryAddRequest country_add_request = new CountryAddRequest()
+            {
+                CountryName = "KSA"
+            };
+            CountryResponse country_response_from_add = await _countriesService.AddCountry(country_add_request);
+
+            //Act
+            CountryResponse? country_response_from_get = await _countriesService.GetCountryByCountryID(country_response_from_add.CountryID);
+
+            //Assert
+            Assert.Equal(country_response_from_add, country_response_from_get);
+        }
+        #endregion
+    }
+    */
+}
