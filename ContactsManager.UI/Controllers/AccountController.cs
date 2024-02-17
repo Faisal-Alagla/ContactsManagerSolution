b50f5a1361261620cbf864ee27a4feff67a19293
Faisal <Faisal@DESKTOP-70EEDMP>
2024-02-17 12:02:37 +0300

    Register View

---
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using ContactsManager.Core.DTO;
+using CRUD_Example.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            //TODO: register the user to the database
+
+            return RedirectToAction(nameof(PersonsController.Index), "PersonsController");
+        }
+    }
+}
