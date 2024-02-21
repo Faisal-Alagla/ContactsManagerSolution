058c1d311fca834ab1fff8d2abefb2dd25a93346
Faisal <Faisal@DESKTOP-70EEDMP>
2024-02-21 11:23:46 +0300

    Role-based authentication

--- ContactsManager.UI/Areas/Admin/Controllers/HomeController.cs
+++ ContactsManager.UI/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsManager.UI.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")] //can applied to action methods as well
     public class HomeController : Controller
     {
         //[Route("admin/home/index")]
